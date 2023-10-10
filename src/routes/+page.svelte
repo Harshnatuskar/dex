6a879e4e5c7e4bbad59d409b70dc3791cbdeeaa2
harshnatuskar <harshnatuskar10@gmail.com>
2023-10-10 23:34:55 +0530

    initial  commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts"> 
+   const greetingObject ={
+    greeting : "How are you",
+    day: "today?"
+   }
+
+   const data = [
+    "oragen",
+    "purple",
+    "olive"
+   ]
+</script>
+
+<h1>{greetingObject.greeting} doing {greetingObject.greeting}!</h1>
+
+{#each data as color,i (color)}  
+    <div>{i}:{color}</div>
+{/each}
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
