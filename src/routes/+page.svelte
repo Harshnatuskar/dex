2b6c6874791f9ca0d3f9f50508ba551964e1e1b6
harshnatuskar <harshnatuskar10@gmail.com>
2023-10-16 13:36:12 +0530

    added: reusable components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,105 @@
-<script lang="ts"> 
-   const greetingObject ={
-    greeting : "How are you",
-    day: "today?"
-   }
+<script lang="ts">
+    import type { PageData } from "./$types"; 
+    import { generations } from "./generations";
+    import {goto} from "$app/navigation";
+    import { page } from "$app/stores"; 
+    import Monster from "./Monster.svelte";
+    
+    export let data: PageData;
+     
+    $: monsterId =$page.url.searchParams.get("monsterId") || '';
 
-   const data = [
-    "oragen",
-    "purple",
-    "olive"
-   ]
-</script>
+    $: monster = data.monsters.find((monster)=> monster.id === monsterId);
+    
+    $: monsterId2 =$page.url.searchParams.get("monsterId2") || '';
 
-<h1>{greetingObject.greeting} doing {greetingObject.greeting}!</h1>
+    $: monster2 = data.monsters.find((monster)=> monster.id === monsterId2);
+    
+    const updateSearchParams = (key: string, value: string) => {
+        const searchParams = new URLSearchParams($page.url.searchParams);
+        searchParams.set(key, value);
+        goto(`?${searchParams.toString()}`);
+    }
+     
+    </script>
 
-{#each data as color,i (color)}  
-    <div>{i}:{color}</div>
-{/each}
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+    {#if monster}
+        <Monster
+            monster={monster} 
+            updateSearchParams={updateSearchParams}
+        />
+    {/if}
+
+    {#if monster2}
+        <Monster
+            monster={monster2} 
+            updateSearchParams={updateSearchParams}
+        />
+    {/if}
+      
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="generations">
+      <a
+        class="generation"
+        class:active={$page.url.searchParams.get('generation') === null}
+        href="/"
+      >
+        All
+      </a>
+      {#each generations as generation (generation.id)}
+        <a
+          class="generation"
+          class:active={$page.url.searchParams.get('generation') === generation.id.toString()}
+          href="/?generation={generation.id}"
+        >
+          {generation.main_region}
+        </a>
+      {/each}
+    </div>
+    
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div class="monsters" >
+      {#each data.monsters as monster (monster.id)}
+         <Monster 
+         monster={monster}
+         updateSearchParams={updateSearchParams}
+         isInteractive={true}
+
+         />
+      {/each}
+    </div>
+    
+    <style>
+        .monsters {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+      }
+      .generations {
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: center;
+      }
+      .generation {
+        margin: 5px;
+        padding: 5px 10px;
+        border: 1px solid black;
+        background-color: #f9f9f9;
+        color: #333; 
+      }
+      .generation.active {
+        background-color: #333;
+        color: #f9f9f9;
+      }
+      .generation:hover {
+        background-color: #eee;
+      }
+      .generation.active:hover {
+        background-color: #555;
+      }
+       
+    
+    </style>
---
+++ src/routes/Monster.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    import type { IndexMonster } from "./+page";
+    export let monster: IndexMonster;
+    export let updateSearchParams: (key: string, value: string) => void;
+    export let isInteractive: boolean = false;
+
+     
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div class="monster" >
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    <div on:click={()=> isInteractive?updateSearchParams('monsterId',monster.id):()=>{}}>
+      <div class="monster-content">
+        <img src={monster.image} alt={monster.name} />
+        {monster.name}
+      </div>
+      <div class="monster-id">
+        {monster.id}
+      </div>
+    </div>
+    <!-- svelte-ignore a11y-no-static-element-interactions -->
+    {#if isInteractive }
+        <div on:click={()=>updateSearchParams("monsterId2",monster.id)}>
+    Add monster 2
+    </div>
+    {/if}
+    
+    </div>
+
+    <style>
+         
+      .monster-id {
+        position: absolute;
+        top: 8px;
+        left: 8px;
+        font-size: 0.8em;
+        color: #aaa;
+      }
+    .monster {
+      text-decoration: none;
+      width: 100px;
+      margin: 10px;
+      padding: 10px;
+      position: relative;
+      background-color: #eee;
+    }
+    .monster:hover {
+      background-color: #ddd;
+    }
+    .monster-content {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    </style>
