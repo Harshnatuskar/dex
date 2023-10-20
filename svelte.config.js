3074e6b707bbe6d6d217810d3c7e788f3f51533e
harshnatuskar <harshnatuskar10@gmail.com>
2023-10-20 14:15:27 +0530

    Deploy to GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: '/dex/' // Replace with your repository name
+			base: '/dex' // Replace with your repository name
 		  },
 	}
 };
